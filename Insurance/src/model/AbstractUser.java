447b9e6edebf6251f2cc51d77bdee8f4f6f33da0
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2017-01-30 12:25:46 +0100

    init insurance

---
+++ Insurance/src/model/AbstractUser.java
@@ -0,0 +1,65 @@
+package model;
+
+@Entity
+public class AbstractUser {
+
+    protected Long id;
+
+    protected String firstName;
+
+    protected String lastName;
+
+    protected String email;
+
+    protected String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+    
+}
